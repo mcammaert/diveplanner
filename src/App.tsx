3efe899ad2f67db952b8f285743cd74625c8fe2f
Manu Cammaert <manu@cammaert.eu>
2017-05-17 22:00:21 +0200

    Remove semicolons
    Signed-off-by: Manu Cammaert <manu@cammaert.eu>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import * as React from 'react';
-import './App.css';
+import * as React from 'react'
+import './App.css'
+import {SacCalculator} from './components/saccalculator'
 
-const logo = require('./logo.svg');
+const logo = require('./logo.svg')
 
 class App extends React.Component<{}, null> {
   render() {
@@ -11,12 +12,10 @@ class App extends React.Component<{}, null> {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <SacCalculator />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
